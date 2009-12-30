61a4f614206e82b24d2e2aa271f5e3da467f367f
zhao lu <zlu@zlus-mbp.rd.francetelecom.fr>
2009-12-30 09:46:15 -0800

    removed emacs backup files and a version of p7 that is almost twice as fast

--- p7.rb
+++ p7.rb
@@ -1,15 +1,15 @@
 # Problem 7
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6^(th) prime is 13.
 # What is the 10001^(st) prime number?
+t1 = Time.now
 p = [2, 3, 5, 7, 11, 13]
-(p.max .. 1000000000).each do |i|
-puts p.max
+(p.max .. 1000000).step(2) do |i|
   j = 0
-  while (j < p.length) && (i % p[j] != 0) do
+  while (j < (p.length/2).ceil) && (i % p[j] != 0) do
     j = j + 1
   end
 
-  if j == p.length
+  if j == (p.length/2).ceil
     p << i
   end
 
@@ -18,3 +18,5 @@ puts p.max
     break
   end
 end
+
+puts "#{Time.now - t1}".to_s + " seconds"
